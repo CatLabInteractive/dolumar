c39d100fcdc7e284e70b4efb9255a68aa09f2a6e
Thijs Van der Schaeghe <thijs@catlab.be>
2015-08-11 12:36:30 +0200

    Trying to get the heroku cronjobs working.

--- cronjobs.php
+++ cronjobs.php
@@ -25,10 +25,12 @@ require_once 'bootstrap/bootstrap.php';
 
 $lock = Neuron_Core_Lock::getInstance();
 
+$cmd = "/app/php/bin/php";
+
 if ($lock->setLock('cronjobs', 60, 60)) {
-	exec ('php cron/constantly.php');
+	exec ($cmd . ' cron/constantly.php');
 }
 
 if ($lock->setLock('cronjobs', 60 * 60 * 24, 60 * 60 * 24)) {
-	exec ('php cron/daily.php');
+	exec ($cmd . ' cron/daily.php');
 }
