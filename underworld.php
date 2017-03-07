1f6e7f3fe57ba318875491e27b4093eee77dd352
daedeloth <thijs@catlab.be>
2017-03-07 16:38:45 +0100

    damn you wiki.

--- underworld.php
+++ underworld.php
@@ -39,7 +39,7 @@ $mission_id = Neuron_Core_Tools::getInput ('_GET', 'id', 'int');
 $mission = Dolumar_Underworld_Mappers_MissionMapper::getFromId ($mission_id);
 $game = new Dolumar_Underworld_Game ($mission);
 
-$server = Neuron_GameServer::getInstance ();
+$server = Neuron_GameServer::bootstrap();
 $server->setGame ($game);
 
 $server->setDispatchURL (ABSOLUTE_URL . 'underworld.php?id='.$mission_id.'&module=');
