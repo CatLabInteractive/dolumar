da68a75ce407b3fc27fb642ec2062663b8e65675
Thijs Van der Schaeghe <thijs@catlab.be>
2018-03-19 13:41:05 +0100

    Update server.

--- src/Dolumar/Underworld/Map/BackgroundManager.php
+++ src/Dolumar/Underworld/Map/BackgroundManager.php
@@ -110,7 +110,7 @@ class Dolumar_Underworld_Map_BackgroundManager
 			else
 			{
 				// We load thze map
-				$map_path = MAPS_PATH . 'maps/' . $this->name;
+				$map_path = MAPS_PATH . $this->name;
 
 				$profiler->start ('Loading ' . $map_path);
 
